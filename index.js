0cd7da12fbef0f80dc5360fc1356297589ebcac1
sudo-stanly <alys.git24@gmail.com>
2026-01-04 21:16:58 +0800

    pop-up alert

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const form_window = document.getElementById("form-window");
 const overlay = document.getElementById("overlay-effect");
 
 const form = document.getElementById("form");
-const mode = null;
+let mode = null;
+let isFieldEmpty = false;
 
 let data={
     title:"",
     description:"",
     date:""
 }
+
+const pop_up = document.getElementById("pop-up-container-id");
+let pop_up_content = document.getElementById("pop-up-box-id");
+let alertType = null;
 
 
 function openFormWindow(x){
@@ -21,8 +26,10 @@ function openFormWindow(x){
             form_window.classList.toggle("toggle-window");
             fab_btn.disabled=true;
 
-            create.innerHTML="";
-            create.innerHTML+=`
+            mode="create";
+
+            form.innerHTML="";
+            form.innerHTML+=`
                 <div class="row">
                     <p><span>Create Todo</span></p>
                     <hr>
@@ -60,34 +67,67 @@ function cancel_creating(){
     overlay.classList.remove("toggle-overlay");
     form_window.classList.remove("toggle-window");
     fab_btn.disabled=false;
-    create.innerHTML="";
+    form.innerHTML="";
 }
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    try{
-        console.log("Validating Form");
-        
-        const inputs = document.querySelectorAll("input");
-        inputs.foreach((input)=>{
-            console.log(input.value);
-        });
+    switch(mode){
+        case 'create':
+            try{
+                console.clear();
+                console.info("Todo created.");
 
+                let inputs = document.querySelectorAll("input");
+                inputs.forEach((input)=>{
+                    console.info(input);
 
-        return true;
+                    if(input.value.trim() === ""){
+                        console.error("Fields are empty.");
+                        isFieldEmpty=true;
+                    }
+                });
+                if(isFieldEmpty===true){
+                    alertType="empty_field";
+                    pop_up_alert();
+                }
+                else{
+                    console.log();
+                }
+
+                return true;
+            }
+            catch{
+                console.clear();
+                console.warn("Invalid form.");
+                return false;
+            }           
+        default:
+            console.error("Invalid parameter.");
+            break;
     }
-    catch{
-        console.clear();
-        console.warn("Invalid Form");
-        return false;
-    }
-    // finally{
-    //     console.info("Todo list created!");
-
-    //     overlay.classList.remove("toggle-overlay");
-    //     form_window.classList.remove("toggle-window");
-    //     fab_btn.disabled=false;
-    //     create.innerHTML="";
-
-    //     return true;
-    // }
+    return;
 });
+function pop_up_alert(){
+    console.warn("Form is Invalid");
+
+    pop_up_content.innerHTML="";
+    switch(alertType){
+        case "empty_field":
+            pop_up.classList.toggle("toggle-pop-up");
+            pop_up_content.innerHTML+=`
+                <div>
+                    <p><span> ⚠ Please fill all required fields. </span></p>
+                </div>
+            `;
+            setTimeout(()=>{
+                console.info("pop-up hidden");
+                pop_up.classList.remove("toggle-pop-up");
+                pop_up_content.innerHTML="";
+            },3000);
+            break;
+        default:
+            console.error("Invalid Parameter.");
+            break;
+    }
+
+}
